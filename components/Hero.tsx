06925f16e9a3991eda0f81440902cf52d7105456
Akshat <akshath.centaurus@gmail.com>
2024-04-24 00:54:21 +0530

    fixed pageInfo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -36,14 +36,14 @@ const Hero=()=> {
           <Link href="#about">
              <button className="heroButton">About</button>
           </Link>
-          <Link href="#experience">
-             <button className="heroButton">Experience</button>
+          <Link href="#services">
+             <button className="heroButton">Services Offered</button>
           </Link>
           <Link href="#skills">
              <button className="heroButton">Skills</button>
           </Link>
-          <Link href="#projects">
-             <button className="heroButton">Projects</button>
+          <Link href="#team">
+             <button className="heroButton">Team Members</button>
           </Link>
         </div>
       </div>
