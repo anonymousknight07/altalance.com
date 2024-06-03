bd84d38cd8d56a41025073dc267553cc18ca55fe
Akshat <akshath.centaurus@gmail.com>
2024-06-03 10:36:32 +0530

    fixed bugs

--- pages/recruit.tsx
+++ pages/recruit.tsx
@@ -26,7 +26,7 @@ const Recruit: React.FC = () => {
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#03abfff7]/80 relative">
       <div 
         className="absolute inset-0 bg-no-repeat bg-center bg-cover opacity-10"
-        style={{ backgroundImage: `url('/images/logo5.png')` }}
+        style={{ backgroundImage: `url('")` }}
       ></div>
       <div className="flex justify-center items-center p-4 relative z-10">
         <iframe
